6e3a0f4803a0f20ea179a485025830491713dfb8
Ilrann <abaevlleonid.m@mail.ru>
2024-03-25 23:17:09 +0300

    defolt

--- maintest.py
+++ maintest.py
@@ -1,6 +1,24 @@
 import telebot
 from telebot import types
 import time
+import sqlite3
+import random
+
+db = sqlite3.connect('database.db', check_same_thread=False)
+
+# Create cursor
+cursor = db.cursor()
+#cursor.execute("""CREATE TABLE results (
+#    username text,
+#    id integer,
+#    result_lite integer,
+#    result_medium integer,
+#    result_hard integer,
+#    global_score
+#)""")
+
+last_test_time = {}
+
 
 # Ваш токен, полученный у BotFather
 TOKEN = '6503039781:AAFOqof_Ve6phNTUIO3BGsyx09CJT8ZTk_o'
@@ -19,6 +37,8 @@ last_answer_time = {}
 
 # Словарь для хранения результатов теста
 results = {}
+
+usersss=set()
 
 # Вопросы и варианты ответов
 questions = {
@@ -83,32 +103,28 @@ questions = {
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
-def handle_start(message):
-    user_id = message.from_user.id
-    results[user_id] = {
-        "Легкие вопросы о географии Европы": {"correct": 0, "total": 0},
-        "Средние вопросы о географии Европы": {"correct": 0, "total": 0},
-        "Сложные вопросы о географии Европы": {"correct": 0, "total": 0},
-    }
+def handle_start(call):
+    if f'@{call.from_user.username} {call.from_user.first_name}' not in usersss:
+        usersss.add(f'@{call.from_user.username} {call.from_user.first_name}')
+        print(f'@{call.from_user.username} {call.from_user.first_name}')
     markup = types.InlineKeyboardMarkup()
     start_test_button = types.InlineKeyboardButton("Начать тест", callback_data="start_test")
-    markup.add(start_test_button)
-    bot.send_message(message.chat.id, f"Главное меню:\nЕсли бот перестал отвечать😊-/start", reply_markup=markup)
-    print(f'@{message.from_user.username}',message.from_user.first_name)
-
-
-@bot.message_handler(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, "ПРЕЖДЕ ЧЕМ ЧТО ТО ТЫКАТЬ ПРОПИШИ /start😊")
+    leaderboard_button = types.InlineKeyboardButton("🏆Таблица Лидеров", callback_data="leaderboard")
+    markup.add(start_test_button, leaderboard_button)
+    bot.send_message(call.from_user.id, f"Главное меню:\nЕсли бот перестал отвечать😊-/start", reply_markup=markup)
 
 
 @bot.callback_query_handler(func=lambda call: call.data == "start_test")
 def start_test_callback(call):
     user_id = call.from_user.id
+    results[user_id] = {
+        "Легкие вопросы о географии Европы": {"correct": 0, "total": 0},
+        "Средние вопросы о географии Европы": {"correct": 0, "total": 0},
+        "Сложные вопросы о географии Европы": {"correct": 0, "total": 0},
+    }
     results[user_id]["current_section"] = None
     results[user_id]["current_question"] = 0  # Обнуляем индекс текущего вопроса
     choice_level(call.message)
-
 
 def choice_level(message):
     markup = types.InlineKeyboardMarkup()
@@ -119,102 +135,252 @@ def choice_level(message):
     bot.send_message(message.chat.id, f"Выберите уровень сложности", reply_markup=markup)
 
 
-@bot.callback_query_handler(func=lambda call: call.data in ["Легкие вопросы о географии Европы", "Средние вопросы о географии Европы", "Сложные вопросы о географии Европы"])
+@bot.callback_query_handler(func=lambda call: call.data in ["Легкие вопросы о географии Европы", "Средние вопросы о географии Европы","Сложные вопросы о географии Европы"])
 def handle_section_choice(call):
-    user_id = call.from_user.id
-    results[user_id]["current_section"] = call.data
-    results[user_id]["current_question"] = 0  # Добавим индекс текущего вопроса
-    bot.send_message(call.message.chat.id, f"Вы выбрали раздел '{results[user_id]['current_section']}'.")
-    send_next_question(call.message.chat.id, user_id)
-
+    try:
+        user_id = call.from_user.id
+        results[user_id]["current_section"] = call.data
+        results[user_id]["current_question"] = 0  # Добавим индекс текущего вопроса
+        text = f"Вы выбрали раздел <b><i>'{results[user_id]['current_section']}'.</i></b>"
+        bot.send_message(call.message.chat.id, text, parse_mode="HTML")
+        send_next_question(call.message.chat.id, user_id)
+    except:
+        bot.send_message(call.from_user.id, "Что-то пошло не так. Пропишите /start")
 
 def send_next_question(chat_id, user_id):
-    if user_id not in results:
-        return
+    try:
+        if user_id not in results:
+            return
 
-    current_section = results[user_id]["current_section"]
-    current_question = results[user_id]["current_question"]
+        current_section = results[user_id]["current_section"]
+        current_question = results[user_id]["current_question"]
 
-    if current_section in questions and 0 <= current_question < len(questions[current_section]):
-        question = questions[current_section][current_question]["question"]
-        answers = questions[current_section][current_question]["answers"]
+        if current_section in questions and 0 <= current_question < len(questions[current_section]):
+            question = questions[current_section][current_question]["question"]
+            answers = questions[current_section][current_question]["answers"]
 
-        markup = types.InlineKeyboardMarkup()
-        for i, answer in enumerate(answers):
-            button = types.InlineKeyboardButton(answer, callback_data=f"answer_{i}")
-            markup.add(button)
+            markup = types.InlineKeyboardMarkup()
+            for i, answer in enumerate(answers):
+                button = types.InlineKeyboardButton(answer, callback_data=f"answer_{i}")
+                markup.add(button)
 
-        bot.send_message(chat_id, f"Вопрос {current_question + 1}:\n{question}", reply_markup=markup)
+            bot.send_message(chat_id, f"Вопрос {current_question + 1}:\n{question}", reply_markup=markup)
+    except:
+        bot.send_message(chat_id, "Что-то пошло не так. Пропишите /start")
 
 
 @bot.callback_query_handler(func=lambda call: call.data.startswith('answer_'))
 def handle_answer(call):
-    user_id = call.from_user.id
-    if user_id not in results:
-        bot.send_message(call.message.chat.id, "Тест не был начат. Пожалуйста, начните тест снова.")
-        return
-
-    current_section = results[user_id]["current_section"]
-    current_question = results[user_id]["current_question"]
-
-    if current_section not in questions:
-        bot.send_message(call.message.chat.id, "Тест завершен. Вы можете начать новый тест.")
-        return
-
-    # Проверяем, когда был отправлен последний ответ
-    if user_id in last_answer_time:
-        current_time = time.time()
-        time_difference = current_time - last_answer_time[user_id]
-        if time_difference < 2:
-            bot.send_message(call.message.chat.id, f"Подождите перед отправкой следующего ответа.😴")
+    try:
+        user_id = call.from_user.id
+        if user_id not in results:
+            bot.send_message(call.message.chat.id, "Что-то пошло не так. Пропишите /start")
             return
 
-    answer_index = int(call.data.split('_')[1])
-    correct_answer = questions[current_section][current_question]["correct_answer"]
-    if answer_index == correct_answer:
-        results[user_id][current_section]["correct"] += 1
-        response = "Правильно! 🎉"
-    else:
-        response = "Неправильно. 😔"
+        current_section = results[user_id]["current_section"]
+        current_question = results[user_id]["current_question"]
 
-    bot.send_message(call.message.chat.id, response)
+        if current_section not in questions:
+            bot.send_message(call.message.chat.id, "Тест завершен. Вы можете начать новый тест.")
+            return
 
-    current_question += 1
-    results[user_id]["current_question"] = current_question  # Обновляем индекс текущего вопроса
+        # Проверяем, когда был отправлен последний ответ
+        if user_id in last_answer_time:
+            current_time = time.time()
+            time_difference = current_time - last_answer_time[user_id]
+            if time_difference < 1:
+                bot.send_message(call.message.chat.id, f"Подождите перед отправкой следующего ответа.😴")
+                return
 
-    # Записываем время отправки текущего ответа
-    last_answer_time[user_id] = time.time()
+        answer_index = int(call.data.split('_')[1])
+        correct_answer = questions[current_section][current_question]["correct_answer"]
+        if answer_index == correct_answer:
+            results[user_id][current_section]["correct"] += 1
+            response = "Правильно! 🎉"
+        else:
+            response = "Неправильно. 😔"
 
-    if current_question < len(questions[current_section]):
-        send_next_question(call.message.chat.id, user_id)
-    else:
-        show_results(call.message.chat.id, user_id, call)
+        bot.send_message(call.message.chat.id, response)
+
+        current_question += 1
+        results[user_id]["current_question"] = current_question  # Обновляем индекс текущего вопроса
+
+        # Записываем время отправки текущего ответа
+        last_answer_time[user_id] = time.time()
+
+        if current_question < 10:
+            send_next_question(call.message.chat.id, user_id)
+        else:
+            show_results(call.message.chat.id, user_id, call)
+    except:
+        bot.send_message(call.from_user.id, "Что-то пошло не так. Пропишите /start")
 
 
-def show_results(chat_id, user_id,call):
-    current_section = results[user_id]["current_section"]
-    correct = results[user_id][current_section]["correct"]
-    total = len(questions[current_section])
-    result_text = f"Правильных ответов: {correct} из {total}"
-    markup = types.InlineKeyboardMarkup()
-    btn_1 = types.InlineKeyboardButton("Главное меню", callback_data="Main menu")
-    markup.add(btn_1)
-    bot.send_message(chat_id, result_text, reply_markup=markup)
-    print(f'@{call.from_user.username}', call.from_user.first_name, f"{current_section}: {correct} из {total}")
+def show_results(chat_id, user_id, call):
+    try:
+        current_section = results[user_id]["current_section"]
+        correct = results[user_id][current_section]["correct"]
+        total = len(questions[current_section])
+        result_text = f"Правильных ответов: {correct} из {total}"
+        markup = types.InlineKeyboardMarkup()
+        btn_1 = types.InlineKeyboardButton("Главное меню", callback_data="mainmenu")
+        markup.add(btn_1)
+        # Функция для добавления или обновления результатов пользователя
+        cursor.execute("SELECT * FROM results WHERE id=?", (chat_id,))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            # Если пользователь уже существует
+            easy_results = results[user_id]['Легкие вопросы о географии Европы']['correct'] + existing_user[2]
+            medium_results = results[user_id]['Средние вопросы о географии Европы']['correct'] + existing_user[3]
+            hard_results = results[user_id]['Сложные вопросы о географии Европы']['correct'] + existing_user[4]
+            if current_section=="Легкие вопросы о географии Европы":
+                total_score = correct * 0.5 + existing_user[5]
+            elif current_section == "Средние вопросы о географии Европы":
+                total_score = correct + existing_user[5]
+            else:
+                total_score = correct * 1.5 + existing_user[5]
+            cursor.execute('''
+                    UPDATE results
+                    SET result_lite=?, result_medium=?, result_hard=?, global_score=?
+                    WHERE id=?
+                ''', (easy_results, medium_results, hard_results, total_score, chat_id))
+        else:
+            # Если пользователь отсутствует, добавьте его в базу данных
+            total_score = results[user_id]['Легкие вопросы о географии Европы']['correct'] * 0.5 + results[user_id]['Средние вопросы о географии Европы']['correct'] + results[user_id]['Сложные вопросы о географии Европы']['correct'] * 1.5
+            cursor.execute('''
+                    INSERT INTO results (id, username, result_lite, result_medium, result_hard, global_score)
+                    VALUES (?, ?, ?, ?, ?, ?)
+                ''', (chat_id, call.from_user.username,
+                      results[user_id]['Легкие вопросы о географии Европы']['correct'],
+                      results[user_id]['Средние вопросы о географии Европы']['correct'],
+                      results[user_id]['Сложные вопросы о географии Европы']['correct'],
+                      total_score))
+        db.commit()
+        if current_section == "Легкие вопросы о географии Европы":
+            section_score = correct * 0.5
+        elif current_section == "Средние вопросы о географии Европы":
+            section_score = correct
+        else:
+            section_score = correct * 1.5
+        bot.send_message(chat_id, f"{result_text} \n+{section_score}ОЧКОВ", reply_markup=markup)
+        print(f'@{call.from_user.username}', call.from_user.first_name, f"{current_section}: {correct} из {total}")
+    except:
+        bot.send_message(call.from_user.id, "Что-то пошло не так. Пропишите /start")
 
 
-@bot.callback_query_handler(func=lambda call: call.data == "Main menu")
+@bot.callback_query_handler(func=lambda call: call.data == "leaderboard")
+def handle_callback(call):
+    try:
+        username_to_find = call.from_user.username
+        cursor.execute("SELECT username, global_score FROM results ORDER BY global_score DESC")
+        leaderboard = cursor.fetchall()
+        if leaderboard!=[]:
+            top5 = leaderboard[:5]  # Первые пять мест
+            position = next((i + 1 for i, (username, _) in enumerate(leaderboard) if username == username_to_find), None)
+            response = []
+            medals = ["🥇", "🥈", "🥉"]  # Смайлики для золотой, серебрянной и бронзовой медалей
+            for i, (username, score) in enumerate(top5, start=1):
+                if i <= 3:
+                    response.append(f"{medals[i-1]} {i} место - {username} {score} очков")
+                else:
+                    response.append(f"{i} место - {username} {score} очков")
+
+            if position is None and leaderboard:
+                response.append(f"\nПохоже вы еще не проходили тест. Вы не найдены в таблице лидеров.")
+            elif position > 5:
+                response.append(f"{position} место - {username_to_find} {leaderboard[position - 1][1]} очков")
+
+            message_text = "\n".join(response)
+
+            # Редактируем сообщение, на которое была нажата кнопка "leaderboard"
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message_text, parse_mode='HTML')
+
+            # Меняем клавиатуру под редактированным сообщением
+            markup = types.InlineKeyboardMarkup()
+            btn_1 = types.InlineKeyboardButton("Главное меню", callback_data="mainmenu")
+            markup.add(btn_1)
+            bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id, reply_markup=markup)
+        else:
+            markup = types.InlineKeyboardMarkup()
+            btn_1 = types.InlineKeyboardButton("Главное меню", callback_data="mainmenu")
+            markup.add(btn_1)
+            message_text = "Лидеров еще нет! Пройди тест и займи 1 место!"
+            bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text=message_text, parse_mode='HTML')
+            bot.edit_message_reply_markup(chat_id=call.message.chat.id, message_id=call.message.message_id,reply_markup=markup)
+    except:
+        bot.send_message(call.from_user.id, "Что-то пошло не так. Пропишите /start")
+
+
+@bot.callback_query_handler(func=lambda call: call.data == "mainmenu")
 def start_test_callback(call):
-    markup = types.InlineKeyboardMarkup()
-    start_test_button = types.InlineKeyboardButton("Начать тест", callback_data="start_test")
-    markup.add(start_test_button)
-    bot.send_message(call.message.chat.id, "Главное меню:", reply_markup=markup)
+    bot.delete_message(chat_id=call.from_user.id, message_id=call.message.message_id)
+    handle_start(call)
 
 
-@bot.message_handler(commands=['update top-list#228192228192'])
-def toplist(call):
-    bot.send_message(call.message.chat.id, "Добро пожаловать! Пожалуйста, укажите свое имя, фамилию, город и часовой пояс.")
+#@bot.message_handler(commands=['help'])
+#def help(message):
+#    bot.send_message(message.chat.id, "ПРЕЖДЕ ЧЕМ ЧТО ТО ТЫКАТЬ ПРОПИШИ /start🤬")
+
+
+@bot.message_handler(commands=['deletedb'])
+def handle_deletedb(message):
+    # Проверьте, что команду отправил администратор, если необходимо
+    if message.from_user.username == 'dihloridus':
+        # Подключение к базе данных
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        # Очистка таблицы результатов (замените 'results' на имя вашей таблицы)
+        cursor.execute("DELETE FROM results")
+
+        # Сохраните изменения и закройте соединение
+        conn.commit()
+        conn.close()
+
+        bot.send_message(message.chat.id, "Данные в базе данных очищены.")
+    else:
+        bot.send_message(message.chat.id, "У вас нет разрешения на выполнение этой команды.")
+
+
+@bot.message_handler(commands=['users'])
+def users_list(message):
+    if message.from_user.username == 'dihloridus':
+        print(usersss)
+        bot.send_message(message.chat.id, "Команда успешно выполнена")
+
+
+@bot.message_handler(commands=['db'])
+def dbcheck(message):
+    if message.from_user.username == 'dihloridus':
+        cursor.execute("SELECT * FROM results ")
+        print(cursor.fetchall())
+        bot.send_message(message.chat.id, "Команда успешно выполнена")
+
+
+# Обработчик команды /addscore
+@bot.message_handler(commands=['addscore'])
+def handle_add_score(message):
+    try:
+        if message.from_user.username == 'dihloridus':
+            args = message.text.split()
+            username = args[1]
+            score = int(args[2])
+            cursor.execute("SELECT global_score FROM results WHERE username=?", (username,))
+            result = cursor.fetchone()
+            if result:
+                new_score = result[0] + score
+                # Обновите значение очков в базе данных
+                cursor.execute("UPDATE results SET global_score=? WHERE username=?", (new_score, username))
+                db.commit()
+                bot.reply_to(message, f"Пользователь {username} получил {score} очков. Текущий счет: {new_score}")
+            else:
+                bot.reply_to(message, "Пользователь не найден в базе данных.")
+    except:
+        bot.send_message(message.chat.id, "Что-то пошло не так. Пропишите /start")
+
 
 
 # Запускаем бота
 bot.polling(none_stop=True)
+
+db.close()
